1f00b8d6767119ca6201b06cbb73e243ee9fbdd5
itsabugnotafeature <41023820+itsabugnotafeature@users.noreply.github.com>
2022-08-07 21:28:02 -0400

    Add app to main page

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,9 @@
 import { applyMarginTop } from "./utils.js";
+import { App } from "./app.js";
 
 const main = document.getElementById("main");
 
-applyMarginTop(main, "5%");
+const app = App();
+app.render(main);
+
+applyMarginTop(main, "10%");
