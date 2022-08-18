ae323f7d6607cf115250f9b7f7eb11975c2f4227
itsabugnotafeature <41023820+itsabugnotafeature@users.noreply.github.com>
2022-08-17 21:33:57 -0400

    Added server/index.js with auth routes (from example code)

---
+++ server/index.js
@@ -0,0 +1,92 @@
+import 'dotenv/config';
+import express from 'express';
+import expressSession from 'express-session';
+import users from './users.js';
+import auth from './auth.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// We will use __dirname later on to send files back to the client.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(dirname(__filename));
+
+// Create the Express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Session configuration
+const sessionConfig = {
+  // set this encryption key in Heroku config (never in GitHub)!
+  secret: process.env.SECRET || 'SECRET',
+  resave: false,
+  saveUninitialized: false,
+};
+
+// Setup the session middleware
+app.use(expressSession(sessionConfig));
+// Allow JSON inputs
+app.use(express.json());
+// Allow URLencoded data
+app.use(express.urlencoded({ extended: true }));
+// Allow static file serving
+app.use(express.static('client'));
+// Configure our authentication strategy
+auth.configure(app);
+
+// Our own middleware to check if the user is authenticated
+function checkLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    // If we are authenticated, run the next route.
+    next();
+  } else {
+    // Otherwise, redirect to the login page.
+    res.redirect('/login');
+  }
+}
+
+// Handle the URL /login (just output the login.html file).
+app.get('/login', (req, res) =>
+  res.sendFile('client/login.html', { root: __dirname })
+);
+
+// Handle post data from the login.html form.
+app.post(
+  '/login',
+  auth.authenticate('local', {
+    // use username/password authentication
+    successRedirect: '/index.html', // when we login, go to /private
+    failureRedirect: '/login', // otherwise, back to login
+  })
+);
+
+// Handle logging out (takes us back to the login page).
+app.get('/logout', (req, res) => {
+  req.logout(); // Logs us out!
+  res.redirect('/login'); // back to login
+});
+
+// Register URL
+app.get('/register', (req, res) =>
+  res.sendFile('client/register.html', { root: __dirname })
+);
+
+// Like login, but add a new user and password IFF one doesn't exist already.
+// If we successfully add a new user, go to /login, else, back to /register.
+// Use req.body to access data (as in, req.body['username']).
+// Use res.redirect to change URLs.
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  if (users.addUser(username, password)) {
+    res.redirect('/login');
+  } else {
+    res.redirect('/register');
+  }
+});
+
+app.get('*', (req, res) => {
+  res.send('Error');
+});
+
+app.listen(port, () => {
+  console.log(`App now listening at http://localhost:${port}`);
+});
