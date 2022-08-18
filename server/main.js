8b7320639195d80b0bdcad4ebb1a57f86cde02b1
itsabugnotafeature <41023820+itsabugnotafeature@users.noreply.github.com>
2022-08-17 21:33:43 -0400

    Deleted old files

--- server/main.js
+++
@@ -1,9 +0,0 @@
-import { applyMarginTop } from "./utils.js";
-import { App } from "./app.js";
-
-const main = document.getElementById("main");
-
-const app = App();
-app.render(main);
-
-applyMarginTop(main, "10%");
